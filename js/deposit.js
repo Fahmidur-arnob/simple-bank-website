f36647e9d6e836d326a674f3dba98c4f368d05f2
Fahmidur-arnob <arnobdev93@gmail.com>
2022-08-16 13:41:13 +0600

    Done with all the deposit transaction

--- js/deposit.js
+++ js/deposit.js
@@ -2,6 +2,32 @@
 
 document.getElementById('deposit-btn').addEventListener('click', function(){
     const depoField = document.getElementById('deposit-field');
-    const depoAmount = depoField.value;
-    console.log(depoAmount);
+    const newDepoAmount = depoField.value;//string
+    const nDepoAmount = parseFloat(newDepoAmount);
+
+    const depoPrevTotal = document.getElementById('depo-amount-holder');
+
+    const prevDepoTotal = depoPrevTotal.innerText;
+    const pDepoTotal = parseFloat(prevDepoTotal);
+    
+    const currentDepoTotal = pDepoTotal + nDepoAmount;
+
+    depoPrevTotal.innerText = currentDepoTotal;
+
+    const balanceTotal = document.getElementById('balance-amount-holder');
+    const prevBalanceTotal = balanceTotal.innerText;
+    const pBalanceTotal = parseFloat(prevBalanceTotal);
+
+
+    const currentBalanceTotal = pBalanceTotal + nDepoAmount;
+
+    balanceTotal.innerText = currentBalanceTotal;
+
+
+
+
+
+
+    depoField.value = '';
+
 });
